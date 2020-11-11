cb00af515727c20a6e4006ec2c4654cfb325f920
Amit Karmacharya <amitkarmacharya@Amits-MacBook-Pro.local>
2020-11-11 13:09:32 -0500

    Class DB - added features, gets all the row from dept, role and employee table.

--- db/employee_management_db.js
+++ db/employee_management_db.js
@@ -26,6 +26,49 @@ class DB {
         console.log("DB connection Ended");
     }
 
+    getDataFromEmployee() {
+        let query = `SELECT * from employee`;
+        let con = this.con;
+        return new Promise(function (resolve, reject) {
+            con.query(query, function (err, result) {
+                if (err) {
+                    reject(new Error("Error while fetching list of all employees", err));
+                } else {
+                    resolve(result);
+                }
+            });
+        }); 
+    }
+
+    getDataFromDepartment() {
+        let query = `SELECT * from department`;
+        let con = this.con;
+        return new Promise(function (resolve, reject) {
+            con.query(query, function (err, result) {
+                if (err) {
+                    reject(new Error("Error while fetching list of all employees", err));
+                } else {
+                    resolve(result);
+                }
+            });
+        }); 
+    }
+
+    getDataFromRoles() {
+        let query = `SELECT * from role`;
+        let con = this.con;
+         return new Promise(function (resolve, reject) {
+            con.query(query, function (err, result) {
+                if (err) {
+                    reject(new Error("Error while fetching list of all employees", err));
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
+
+
 }
 
 module.exports = DB;
